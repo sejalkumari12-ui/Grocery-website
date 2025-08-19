f9902d690b829bf7d89e943f80a7247864d39a0c
sejal <user.email: sejalkumari5679@gmail.com>
2025-08-19 17:45:31 +0530

    fixed text

--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = (props) => {
   return (
     <div className='w-fit mx-auto '>
-            <h2 className='md:text-5xl text-[2.5rem] font-bold'>
+            <h2 className='md:text-5xl text-[2.5rem] font-bold text-zinc-800'>
                 <span className='text-orange-500'>{props.highlight}</span> {props.heading}</h2>
             <div className='w-34 h-1 bg-orange-400 md:mt-3 mt-4 ml-auto'>
 
